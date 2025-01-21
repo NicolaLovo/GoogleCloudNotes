afd2ac3735f24084ac9c2766207fa6e5227f0cfe
NicolaLovo <nicolalovo@gmail.com>
2025-01-21 16:46:16 +0100

    End terraform first course

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -31,13 +31,13 @@ resource "google_storage_bucket_object" "static_site_src" {
 
 
 # ----------------
-
+/*
 resource "google_compute_global_address" "website_ip" {
   name = "website-lb-ip"
 
 }
 
-# get the dns zone since it already exists
+# get the dns zone since it already exists in gcp
 data "google_dns_managed_zone" "dns_zone" {
   name = "my-dns-zone"
 }
@@ -59,3 +59,37 @@ resource "google_compute_backend_bucket" "website-backend" {
   description = "Contains files needed for the website"
   enable_cdn = true
 }
+
+# gcp url map
+
+resource "google_compute_url_map" "website" {
+  name = "website-url-map"
+  default_service = google_compute_backend_bucket.website-backend.self_link
+  host_rule {
+    hosts = ["*"]
+    path_matcher = "allpaths"
+  }
+  path_matcher {
+    name = "allpaths"
+    default_service = google_compute_backend_bucket.website-backend.self_link
+  }
+}
+
+# gcp http proxy
+
+resource "google_compute_target_http_proxy" "website" {
+  name = "website-proxy"
+  url_map = google_compute_url_map.website.self_link
+}
+
+# gcp forwarding rule
+resource "google_compute_global_forwarding_rule" "default" {
+  name = "website-forwarding-rule"
+  load_balancing_scheme = "EXTERNAL"
+  ip_address = google_compute_global_address.website_ip.address
+  ip_protocol = "TCP"
+  port_range = "80"
+  target = google_compute_target_http_proxy.website.self_link
+}
+
+*/
